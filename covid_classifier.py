08812c7f4f260e6354f59fc2ceaebe18fae74afb
Joost Grunwald <joostgrunwald2001@gmail.com>
2021-08-27 10:00:07 +0200

    made a beginning

--- covid_classifier.py
+++ covid_classifier.py
@@ -23,4 +23,26 @@ bevatten eerste vijf zinnen een coronawoord --> dubbele punten
 dubbele coronawoorden --> dubbele punten
 """
 
+#*################
+#?IMPORT SETTINGS#
+#*################
+import os
 
+#*#########
+#?SETTINGS#
+#*#########
+data_path = "C:\Users\Josso\Desktop\.txt incl. meta\dataset_txt_renamed"
+
+#*###############
+#?ACTUAL PROGRAM#
+#*###############
+#For all files in data path
+for filename in os.listdir(data_path):
+
+    #If filename contains .txt and does not contain the word meta (for seperating metadata)
+    if filename.endswith(".txt") and filename.find("meta") == -1:
+
+
+        #TODO: OPEN FILENAME
+    else:
+        continue
